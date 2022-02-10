a18e723725acb7f2ef7b1d9932a445b145fd7bc0
Kian Milani <kianmilani@wentletrap.hpc.arizona.edu>
2022-02-10 11:16:08 -0700

    run with proper models now

--- cgi.py
+++ cgi.py
@@ -9,9 +9,13 @@ from pathlib import Path
 
 import poppy
 from poppy_roman_cgi_phasec import hlc, spc, run
+
+import proper
+import roman_phasec_proper as phasec
+
 import misc
 
-class CGI():
+class CGI_POPPY():
 
     def __init__(self, cgi_mode='HLC575', wavelength=None, npsf=64, psf_pixelscale=13e-6*u.m/u.pix,
                  use_pupil_defocus=True, use_fieldstop=False, use_opds=False, polaxis=0, quiet=True):
@@ -52,15 +56,6 @@ class CGI():
                                                radius=self.dm_diam/2, influence_func=str(self.dm_dir/'proper_inf_func.fits'))
         
         self.texp = 1
-        
-
-    def set_shutter(self, shutter_state):
-        if shutter_state:
-            self.testbed.nkt_onoff(1)
-            self.testbed.nkt_onoff(1)
-        else:
-            self.testbed.nkt_onoff(0)
-            self.testbed.nkt_onoff(0)
     
     def reset_dms(self):
         self.DM1.set_surface( np.zeros((self.Nact, self.Nact)) )
@@ -116,10 +111,116 @@ class CGI():
             
         psfs, wfs = run.run_multi(ncpus=16, mode=self.cgi_mode, wavelength=self.wavelength, 
                                   npix=self.npix, oversample=self.oversample, 
-                                 npsf=self.npsf, psf_pixelscale=self.psf_pixelscale,
+                                  npsf=self.npsf, psf_pixelscale=self.psf_pixelscale,
                                  dm1=dm1_commands, dm2=dm2_commands,
                                  use_opds=self.use_opds, polaxis=self.polaxis,
                                  use_pupil_defocus=self.use_pupil_defocus, use_fieldstop=self.use_fieldstop,
                                  cgi_dir=self.cgi_dir, quiet=self.quiet)
         return wfs
+    
+class CGI_PROPER():
+
+    def __init__(self, cgi_mode='hlc', wavelength=None, npsf=64, psf_pixelscale=13e-6*u.m/u.pix,
+                 use_pupil_defocus=True, use_fieldstop=False, use_opds=False, polaxis=0, quiet=True):
+        
+        self.cgi_mode = cgi_mode
+        if cgi_mode.find('hlc') != -1: 
+            self.wavelength_c = 575e-9*u.m
+            self.npix = 309
+        elif cgi_mode=='spc-spec':
+            self.wavelength_c = 730e-9*u.m
+        elif cgi_mode=='spc-wide':
+            self.wavelength_c = 825e-9*u.m
+            
+        if wavelength is None: self.wavelength = self.wavelength_c
+        self.npsf = npsf
+        self.psf_pixelscale = psf_pixelscale
+        self.use_pupil_defocus = use_pupil_defocus
+        self.use_fieldstop = use_fieldstop
+        self.use_opds = use_opds
+        self.polaxis = polaxis
+        self.quiet = quiet
+        
+        self.cgi_dir = phasec.data_dir
+        
+        self.Nact = 48
+        self.dm_diam = 46.3*u.mm
+        self.act_spacing = 0.9906*u.mm
+        
+        self.DM1 = np.zeros((self.Nact,self.Nact))
+        self.DM2 = np.zeros((self.Nact,self.Nact))
+        
+        self.texp = 1
+    
+    def reset_dms(self):
+        self.DM1 = np.zeros((self.Nact,self.Nact))
+        self.DM2 = np.zeros((self.Nact,self.Nact))
+        
+    def check_dm_command_shape(self, dm_command):
+        if dm_command.shape[0]==self.Nact**2 or dm_command.shape[1]==self.Nact**2:
+            dm_command = dm_command.reshape((self.Nact, self.Nact))
+        return dm_command
+            
+    def set_dm1(self, dm_command):
+        dm_command = self.check_dm_command_shape(dm_command)
+        self.DM1 = dm_command
+    
+    def set_dm2(self, dm_command):
+        dm_command = self.check_dm_command_shape(dm_command)
+        self.DM2 = dm_command
+        
+    def add_dm1(self, dm_command):
+        dm_command = self.check_dm_command_shape(dm_command)
+        self.DM1 += dm_command
+        
+    def add_dm2(self, dm_command):
+        dm_command = self.check_dm_command_shape(dm_command)
+        self.DM2 += dm_command
+        
+    def calc_psf(self):
+        
+        options = {'cor_type':self.cgi_mode,
+                   'final_sampling_m':self.psf_pixelscale.to(u.m/u.pix).value, 
+                   'use_errors':self.use_opds,
+                   'polaxis':self.polaxis,
+                   'use_field_stop':self.use_fieldstop,
+                   'use_pupil_defocus':self.use_pupil_defocus,
+                   'use_dm1':1, 'dm1_m':self.DM1, 
+                   'use_dm2':1, 'dm2_m':self.DM2, }
+        
+        (wfs, sampling_m) = proper.prop_run_multi('roman_phasec', 
+                                                  self.wavelength.to(u.micron).value, self.npsf, QUIET=self.quiet, 
+                                                  PASSVALUE=options)
+        psf = np.sum(np.abs(wfs)**2, axis=0)
+        return psf
+    
+    def calc_psfs(self, dm1_commands, dm2_commands):
+        ''' dm1_commands and dm2_commands must be lists of np.ndarrays '''
+        
+        options = []
+        for i in range(len(dm1_commands)): 
+            dm1_commands[i] += self.DM1
+            dm2_commands[i] += self.DM2
+            options.append({'cor_type':self.cgi_mode,
+                            'final_sampling_m':self.psf_pixelscale.to(u.m/u.pix).value, 
+                            'use_errors':self.use_opds,
+                            'polaxis':self.polaxis,
+                            'use_field_stop':self.use_fieldstop,
+                            'use_pupil_defocus':self.use_pupil_defocus,
+                            'use_dm1':1, 'dm1_m':dm1_commands[i], 
+                            'use_dm2':1, 'dm2_m':dm2_commands[i], 
+                           })
+            
+        (wfs, sampling_m) = proper.prop_run_multi('roman_phasec', 
+                                                  self.wavelength.to(u.micron).value, self.npsf, QUIET=self.quiet, 
+                                                  PASSVALUE=options)
+        
+        psfs = np.abs(wfs)**2
+        return psfs
+        
+        
+        
+        
+        
+        
         
--- iefc_simulation.py
+++ iefc_simulation.py
@@ -7,39 +7,30 @@ from datetime import date
 import astropy.io.fits as pyfits
 from scipy.sparse import linalg as sLA
 
+import cgi
+
 # def take_measurement(system_interface, probe_cube, probe_amplitude, return_all=False, pca_modes=None):
 def take_measurement(sysi, probe_cube, probe_amplitude, return_all=False, pca_modes=None):
     differential_operator = np.array([ [-1,1,0,0] , [0,0,-1,1] ]) / (2 * probe_amplitude * sysi.texp)
-    
-#     # Measure the response
-#     images = []
-#     for probe in probe_cube:
-#         for s in [-1.0, 1.0]:
-#             # Add the probe to the DM
-# #             system_interface.add_dm(s * probe_amplitude * probe)
-#             sysi.add_dm1(s * probe_amplitude * probe)
-            
-#             # Measure the response
-# #             sysi.send_dm()
-# #             image = system_interface.snap()
-#             image = sysi.calc_psf()
-#             images.append(image.flatten())
-            
-#             # Remove the probe from the DM
-# #             system_interface.add_dm(-s * probe_amplitude * probe)
-#             sysi.add_dm1(-s * probe_amplitude * probe)
 
     dm1_commands = [-1.0*probe_amplitude*probe_cube[0].reshape(sysi.Nact,sysi.Nact),
                     1.0*probe_amplitude*probe_cube[0].reshape(sysi.Nact,sysi.Nact),
                     -1.0*probe_amplitude*probe_cube[1].reshape(sysi.Nact,sysi.Nact),
                     1.0*probe_amplitude*probe_cube[1].reshape(sysi.Nact,sysi.Nact)]
     dm2_commands = [np.zeros((48,48)), np.zeros((48,48)), np.zeros((48,48)), np.zeros((48,48))]
-    wfs = sysi.calc_psfs(dm1_commands, dm2_commands)
     
+    psfs = sysi.calc_psfs(dm1_commands, dm2_commands)
     images=[]
-    for i,wf in enumerate(wfs): images.append(wf.intensity.flatten())
-    
-#     system_interface.send_dm()
+    for i,psf in enumerate(psfs): images.append(psf.flatten())
+#     if isinstance(sysi, cgi.CGI_POPPY):
+#         wfs = sysi.calc_psfs(dm1_commands, dm2_commands)
+#         images=[]
+#         for i,wf in enumerate(wfs): images.append(wf.intensity.flatten())
+#     elif isinstance(sysi, cgi.CGI_PROPER):
+#         psfs = sysi.calc_psfs(dm1_commands, dm2_commands)
+#         images=[]
+#         for i,psf in enumerate(psfs): images.append(wf.intensity.flatten())
+            
     images = np.array(images)
     differential_images = differential_operator.dot(images)
     
@@ -63,7 +54,6 @@ def calibrate(sysi, probe_amplitude, probe_modes, calibration_amplitude, calibra
             # We need a + and - probe to estimate the jacobian
             for s in [-1, 1]:
                 # Set the DM to the correct state
-#                 system_interface.add_dm(s * calibration_amplitude * calibration_mode)
                 sysi.add_dm1(s * calibration_amplitude * calibration_mode)
                 differential_images, single_images = take_measurement(sysi, probe_modes, probe_amplitude, return_all=True)
                 
@@ -71,7 +61,6 @@ def calibrate(sysi, probe_amplitude, probe_modes, calibration_amplitude, calibra
                 images.append(single_images)
                 
                 # Remove the calibrated mode
-#                 system_interface.add_dm(-s * calibration_amplitude * calibration_mode)
                 sysi.add_dm1(-s * calibration_amplitude * calibration_mode)
             print("\tCalibrated mode {:d} / {:d} in {:.3f}s".format(ci+1+start_mode, calibration_modes.shape[0], 
                                                                     time.time()-start))
