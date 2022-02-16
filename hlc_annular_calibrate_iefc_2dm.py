0d537c6e26b520cfd63e203078c9a683330c298f
Kian Milani <kianmilani@junonia.hpc.arizona.edu>
2022-02-16 12:15:20 -0700

    working on 2DM implementation

---
+++ hlc_annular_calibrate_iefc_2dm.py
@@ -0,0 +1,87 @@
+import numpy as np
+import astropy.units as u
+from astropy.io import fits
+from matplotlib.patches import Rectangle
+from pathlib import Path
+from importlib import reload
+from IPython.display import clear_output
+
+from IEFC_modules import iefc_functions
+
+from roman_cgi_iefc import cgi
+from roman_cgi_iefc import iefc_sim as iefc
+from roman_cgi_iefc import iefc_sim_2dm as iefc2
+
+import misc
+
+import proper
+proper.prop_use_fftw(DISABLE=False)
+proper.prop_fftw_wisdom( 1024 ) 
+
+data_dir = Path('/groups/douglase/kians-data-files/roman-cgi-iefc-data')
+flatmaps_dir = Path.home()/'src/pyfalco/roman/flatmaps'
+
+dm1_flatmap = fits.getdata(flatmaps_dir/'dm1_m_flat_hlc_band1.fits')
+dm2_flatmap = fits.getdata(flatmaps_dir/'dm2_m_flat_hlc_band1.fits')
+dm1_design = fits.getdata(flatmaps_dir/'dm1_m_design_hlc_band1.fits')
+dm2_design = fits.getdata(flatmaps_dir/'dm2_m_design_hlc_band1.fits')
+
+
+# Initialize Mode
+hlci = cgi.CGI_PROPER(use_opds=False, use_fieldstop=False, quiet=True)
+npsf = hlci.npsf
+Nact = hlci.Nact
+
+# Create dark hole ROI to calibrate and control
+xfp = np.linspace(-0.5, 0.5, npsf) * npsf * hlci.psf_pixelscale_lamD
+xf,yf = np.meshgrid(xfp,xfp)
+
+edge = 2
+iwa = 3
+owa = 6
+
+# Create the mask that is used to select which region to make dark.
+dh_params = {
+    'inner_radius' : iwa,
+    'outer_radius' : owa,
+    'edge_position' : edge,
+    'direction' : ''
+}
+dh_mask = iefc2.create_dshaped_focal_plane_mask(xf, yf, dh_params).ravel()
+
+#Create the mask that is used to select which region to make dark.
+control_params = {
+    'inner_radius' : iwa-0.5,
+    'outer_radius' : owa+1,
+    'edge_position' : edge,
+    'direction' : ''
+}
+control_mask = iefc2.create_dshaped_focal_plane_mask(xf, yf, control_params).ravel()
+
+relative_weight = 0.99
+weights = dh_mask * relative_weight + (1 - relative_weight) * control_mask
+print(dh_mask.shape, control_mask.shape, control_mask.shape, weights.shape)
+
+# Create probe and fourier modes
+fourier_modes, fx, fy = iefc2.create_fourier_modes(xfp, control_mask.reshape((npsf,npsf)), Nact, circular_mask=False)
+probe_modes = iefc2.create_probe_poke_modes(hlci.Nact, 3*Nact//4, Nact//2, 3*Nact//4-1, Nact//2)
+
+calibration_amplitude = 0.006 * hlci.wavelength.to(u.m).value
+probe_amplitude = 0.05 * hlci.wavelength.to(u.m).value
+print('Calibration modes required: {:d}'.format(fourier_modes.shape[0]))
+
+# Calibrate IEFC
+response_cube, calibration_cube = iefc2.calibrate(hlci, probe_amplitude, probe_modes, 
+                                                  calibration_amplitude, fourier_modes, start_mode=0)
+
+response_hdu = fits.PrimaryHDU(data=response_cube)
+response_hdu.writeto(data_dir/'response-data'/'hlc_response_cube_circ_3to6.fits', overwrite=True)
+
+calib_hdu = fits.PrimaryHDU(data=calibration_cube)
+calib_hdu.writeto(data_dir/'calibration-data'/'hlc_calibration_cube_circ_3to6.fits', overwrite=True)
+
+print('Calibration results saved.')
+
+
+
+
--- roman_cgi_iefc/iefc_sim_2dm.py
+++ roman_cgi_iefc/iefc_sim_2dm.py
@@ -11,14 +11,21 @@ from .import cgi
 import misc
 
 # def take_measurement(system_interface, probe_cube, probe_amplitude, return_all=False, pca_modes=None):
-def take_measurement(sysi, probe_cube, probe_amplitude, return_all=False, pca_modes=None):
+def take_measurement(sysi, probe_cube, probe_amplitude, DM=1, return_all=False, pca_modes=None):
     differential_operator = np.array([ [-1,1,0,0] , [0,0,-1,1] ]) / (2 * probe_amplitude * sysi.texp)
 
-    dm1_commands = [-1.0*probe_amplitude*probe_cube[0].reshape(sysi.Nact,sysi.Nact),
-                    1.0*probe_amplitude*probe_cube[0].reshape(sysi.Nact,sysi.Nact),
-                    -1.0*probe_amplitude*probe_cube[1].reshape(sysi.Nact,sysi.Nact),
-                    1.0*probe_amplitude*probe_cube[1].reshape(sysi.Nact,sysi.Nact)]
-    dm2_commands = [np.zeros((48,48)), np.zeros((48,48)), np.zeros((48,48)), np.zeros((48,48))]
+    if DM==1:
+        dm1_commands = [-1.0*probe_amplitude*probe_cube[0].reshape(sysi.Nact,sysi.Nact),
+                        1.0*probe_amplitude*probe_cube[0].reshape(sysi.Nact,sysi.Nact),
+                        -1.0*probe_amplitude*probe_cube[1].reshape(sysi.Nact,sysi.Nact),
+                        1.0*probe_amplitude*probe_cube[1].reshape(sysi.Nact,sysi.Nact)]
+        dm2_commands = [np.zeros((48,48)), np.zeros((48,48)), np.zeros((48,48)), np.zeros((48,48))]
+    elif DM==2:
+        dm2_commands = [-1.0*probe_amplitude*probe_cube[0].reshape(sysi.Nact,sysi.Nact),
+                        1.0*probe_amplitude*probe_cube[0].reshape(sysi.Nact,sysi.Nact),
+                        -1.0*probe_amplitude*probe_cube[1].reshape(sysi.Nact,sysi.Nact),
+                        1.0*probe_amplitude*probe_cube[1].reshape(sysi.Nact,sysi.Nact)]
+        dm1_commands = [np.zeros((48,48)), np.zeros((48,48)), np.zeros((48,48)), np.zeros((48,48))]
     
     psfs = sysi.calc_psfs(dm1_commands, dm2_commands)
     images=[]
@@ -37,20 +44,24 @@ def take_measurement(sysi, probe_cube, probe_amplitude, return_all=False, pca_mo
     
 def calibrate(sysi, probe_amplitude, probe_modes, calibration_amplitude, calibration_modes, start_mode=0):
     print('Calibrating I-EFC...')
-    slopes = []
-    images = []
+    
+    slopes_1 = []
+    slopes_2 = []
+    images_1 = []
+    images_2 = []
+    
     # Loop through all modes that you want to control
     start = time.time()
     for ci, calibration_mode in enumerate(calibration_modes[start_mode::]):
         try:
-            slope = 0
+            slope1, slope2 = (0, 0)
             for s in [-1, 1]: # We need a + and - probe to estimate the jacobian
                 # DM1: Set the DM to the correct state
                 sysi.add_dm1(s * calibration_amplitude * calibration_mode)
                 differential_images_1, single_images_1 = take_measurement(sysi, probe_modes, probe_amplitude, return_all=True)
                 
-                slope_1 += s * differential_images_1 / (2 * calibration_amplitude)
-                images_1.append(single_images)
+                images_1.append(single_images_1)
+                slope1 += s * differential_images_1 / (2 * calibration_amplitude)
                 
                 sysi.add_dm1(-s * calibration_amplitude * calibration_mode) # remove the calibrated mode from DM1
                 
@@ -58,32 +69,93 @@ def calibrate(sysi, probe_amplitude, probe_modes, calibration_amplitude, calibra
                 sysi.add_dm2(s * calibration_amplitude * calibration_mode)
                 differential_images_2, single_images_2 = take_measurement(sysi, probe_modes, probe_amplitude, return_all=True)
                 
-                slope_2 += s * differential_images_2 / (2 * calibration_amplitude)
-                images_2.append(single_images)
+                images_2.append(single_images_2)
+                slope2 += s * differential_images_2 / (2 * calibration_amplitude)
                 
                 sysi.add_dm2(-s * calibration_amplitude * calibration_mode) # remove the calibrated mode from DM2
                 
                 
             print("\tCalibrated mode {:d} / {:d} in {:.3f}s".format(ci+1+start_mode, calibration_modes.shape[0], 
                                                                     time.time()-start))
-            slopes.append(slope)
+            slopes_1.append(slope1)
+            slopes_2.append(slope2)
         except KeyboardInterrupt: 
             print('Calibration interrupted.')
             break
+    
+    slopes_1 = np.array(slopes_1)
+    slopes_2 = np.array(slopes_2)
+    images_1 = np.array(images_1)
+    images_2 = np.array(images_2)
+
+    slopes = np.concatenate((slopes_1,slopes_2), axis=2) # this is the response cube
+    images = np.concatenate((images_1,images_2), axis=2) # this is the calibration cube
+    
     print('Calibration complete.')
-    return np.array(slopes), np.array(images)
+    return slopes, images
+
+def WeightedLeastSquares(A, W, rcond=1e-15):
+    print(A.T.shape)
+    print(W.dot(A).shape)
+    cov = A.T.dot(W.dot(A))
+    return np.linalg.inv(cov + rcond * np.diag(cov).max() * np.eye(A.shape[1])).dot( A.T.dot(W) )
+
+def TikhonovInverse(A, rcond=1e-15):
+    U, s, Vt = np.linalg.svd(A, full_matrices=False)
+    s_inv = s/(s**2 + (rcond * s.max())**2)
+    return (Vt.T * s_inv).dot(U.T)
+
+def construct_control_matrix(response_matrix, weight_map, rcond=1e-2, pca_modes=None):
+    weight_mask = weight_map>0
+    
+    # Invert the matrix with an SVD and Tikhonov regularization
+#     masked_matrix = response_matrix[:, :, weight_mask].reshape((response_matrix.shape[0], -1)).T
+    masked_matrix = response_matrix[:, :, np.concatenate((weight_mask,weight_mask))].reshape((response_matrix.shape[0], -1)).T
+    
+    # Add the extra PCA modes that are fitted
+    if pca_modes is not None:
+        double_pca_modes = np.concatenate( (pca_modes[:, weight_mask], pca_modes[:, weight_mask]), axis=1).T
+        masked_matrix = np.hstack((masked_matrix, double_pca_modes))
+        
+#     Wmatrix = np.diag(np.concatenate( (weight_map[weight_mask], weight_map[weight_mask]) ) )
+    wmap = weight_map[weight_mask]
+    Wmatrix = np.diag(np.concatenate( (wmap, wmap, wmap, wmap) ) )
+    print(masked_matrix.shape, Wmatrix.shape)
+    control_matrix = WeightedLeastSquares(masked_matrix, Wmatrix, rcond=rcond)
+    
+    if pca_modes is not None:
+        # Return the control matrix minus the pca_mode coefficients
+        return control_matrix[0:-pca_modes.shape[0]]
+    else:
+        return control_matrix
 
 def single_iteration(sysi, probe_cube, probe_amplitude, control_matrix, pixel_mask_dark_hole):
-    # Take a measurement
-    differential_images = take_measurement(sysi, probe_cube, probe_amplitude)
+#     # Take a measurement
+#     differential_images = take_measurement(sysi, probe_cube, probe_amplitude)
     
-    # Choose which pixels we want to control
-    measurement_vector = differential_images[:, pixel_mask_dark_hole].ravel()
+#     # Choose which pixels we want to control
+#     measurement_vector = differential_images[:, pixel_mask_dark_hole].ravel()
+#     print(differential_images.shape, measurement_vector.shape)
+    
+    differential_images_1 = take_measurement(sysi, probe_cube, probe_amplitude, DM=1) # Take a measurement
+    measurement_vector_1 = differential_images_1[:, pixel_mask_dark_hole].ravel() # Choose which pixels we want to control
+    
+    differential_images_2 = take_measurement(sysi, probe_cube, probe_amplitude, DM=2) # Take a measurement
+    measurement_vector_2 = differential_images_2[:, pixel_mask_dark_hole].ravel() # Choose which pixels we want to control
     
     # Calculate the control signal in modal coefficients
-    reconstructed_coefficients = control_matrix.dot( measurement_vector )
+#     reconstructed_coefficients = control_matrix.dot( measurement_vector )
+    reconstructed_coefficients = control_matrix.dot( np.concatenate((measurement_vector_1,measurement_vector_2),axis=0) )
     
     return reconstructed_coefficients
+    
+def remove_k_pca_modes(data_cube, k=2):
+    ''' This function removes the k strongest PCA modes.'''
+    temp_cube = data_cube.reshape((-1, data_cube.shape[-1]))
+    
+    U, s, V = sLA.svds(temp_cube, k=k)
+    filtered_data = temp_cube - (V.T.dot( V.dot(temp_cube.T) )).T
+    return filtered_data.reshape(data_cube.shape), V
 
 def create_fourier_modes(xfp, mask, Nact=48, use_both=True, circular_mask=True):
     print("Creating Fourier modes: ", mask.shape)
@@ -138,56 +210,7 @@ def create_probe_poke_modes(Nact, indx0, indy0, indx1, indy1):
     probe_modes = probe_modes.reshape((2, -1))
     return probe_modes
 
-
-def remove_k_pca_modes(data_cube, k=2):
-    ''' This function removes the k strongest PCA modes.'''
-    temp_cube = data_cube.reshape((-1, data_cube.shape[-1]))
-    
-    U, s, V = sLA.svds(temp_cube, k=k)
-    filtered_data = temp_cube - (V.T.dot( V.dot(temp_cube.T) )).T
-    return filtered_data.reshape(data_cube.shape), V
-
-def WeightedLeastSquares(A, W, rcond=1e-15):
-    cov = A.T.dot(W.dot(A))
-    return np.linalg.inv(cov + rcond * np.diag(cov).max() * np.eye(A.shape[1])).dot( A.T.dot(W) )
-
-def TikhonovInverse(A, rcond=1e-15):
-    U, s, Vt = np.linalg.svd(A, full_matrices=False)
-    s_inv = s/(s**2 + (rcond * s.max())**2)
-    return (Vt.T * s_inv).dot(U.T)
-
-def construct_control_matrix(response_matrix, weight_map, rcond=1e-2, pca_modes=None):
-    weight_mask = weight_map>0
-    
-    # Invert the matrix with an SVD and Tikhonov regularization
-    masked_matrix = response_matrix[:, :, weight_mask].reshape((response_matrix.shape[0], -1)).T
-    
-    # Add the extra PCA modes that are fitted
-    if pca_modes is not None:
-        double_pca_modes = np.concatenate( (pca_modes[:, weight_mask], pca_modes[:, weight_mask]), axis=1).T
-        masked_matrix = np.hstack((masked_matrix, double_pca_modes))
-        
-    Wmatrix = np.diag(np.concatenate((weight_map[weight_mask], weight_map[weight_mask])))
-    control_matrix = WeightedLeastSquares(masked_matrix, Wmatrix, rcond=rcond)
-    
-    if pca_modes is not None:
-        # Return the control matrix minus the pca_mode coefficients
-        return control_matrix[0:-pca_modes.shape[0]]
-    else:
-        return control_matrix
-
-def create_rect_patch(rect_params):
-    rect_patch = Rectangle((rect_params['x0']-rect_params['w']/2, rect_params['y0']-rect_params['h']/2), 
-                           rect_params['w'], rect_params['h'], color='c', fill=False)
-    return rect_patch
-
-def create_circ_patches(circ_params):
-    circ_patches = [Circle( (0,0), circ_params['inner_radius'], color='c', fill=False), 
-                  Circle( (0,0), circ_params['outer_radius'], color='c', fill=False)]
-    return circ_patches
-
-
-def create_dshaped_focal_plane_mask(x, y, params):
+def create_annular_focal_plane_mask(x, y, params):
     inner_radius, outer_radius, edge_position, direction = (params['inner_radius'], params['outer_radius'], 
                                                             params['edge_position'], params['direction'])
     r = np.hypot(x, y)
@@ -204,7 +227,15 @@ def create_box_focal_plane_mask(x, y, params):
     mask = ( abs(x - x0) < width/2 ) * ( abs(y - y0) < height/2 )
     return mask > 0
 
+def create_rect_patch(rect_params):
+    rect_patch = Rectangle((rect_params['x0']-rect_params['w']/2, rect_params['y0']-rect_params['h']/2), 
+                           rect_params['w'], rect_params['h'], color='c', fill=False)
+    return rect_patch
 
+def create_circ_patches(circ_params):
+    circ_patches = [Circle( (0,0), circ_params['inner_radius'], color='c', fill=False), 
+                  Circle( (0,0), circ_params['outer_radius'], color='c', fill=False)]
+    return circ_patches
 
 
 
