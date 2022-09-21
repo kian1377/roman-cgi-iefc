e954b4af1d69496fe470454281f43caf6265d7b7
Kian Milani <kianmilani@arizona.edu>
2022-09-21 14:55:27 -0700

    still working with pwp

--- wfsc/pwp_1dm.py
+++ wfsc/pwp_1dm.py
@@ -164,7 +164,6 @@ def run_pwp_new(sysi, probes, dark_mask, use, jacobian=None, model=None, use_noi
 
         if (use=='jacobian' or use=='j') and jacobian is not None:
             E_probe = jacobian.dot(np.array(probes[i].flatten())) # Use jacobian to model probe E-field at the focal plane
-#             E_probe = E_probe[:nmask] + 1j*E_probe[nmask:]
         elif (use=='model' or use=='m') and model is not None:
             E_full = model.calc_psf().wavefront.get()[dark_mask] if i==0 else E_full
             
@@ -204,6 +203,91 @@ def run_pwp_new(sysi, probes, dark_mask, use, jacobian=None, model=None, use_noi
     
     return E_est_2d
 
+
+def run_pwp_broad(sysi, wavelengths, probes, dark_mask, use, jacobian=None, model=None, use_noise=False, display=False):
+    nmask = dark_mask.sum()
+    nwaves = len(wavelengths)
+    nprobes = probes.shape[0]
+    
+    dm_ref = sysi.get_dm1()
+    amps = np.linspace(-1, 1, 2) # for generating a negative and positive probe
+    
+    Ip = []
+    In = []
+    for i,probe in enumerate(probes):
+        for amp in amps:
+            sysi.add_dm1(amp*probe)
+            
+            bb_im = 0
+            for wavelength in wavelengths:
+                sysi.wavelength = wavelength
+                bb_im += sysi.snap()
+                
+            if amp==-1: 
+                In.append(bb_im)
+            else: 
+                Ip.append(bb_im)
+                
+            sysi.add_dm1(-amp*probe) # remove probe from DM
+            
+        if display:
+            misc.myimshow3(Ip[i], In[i], Ip[i]-In[i],
+                           'Probe {:d} Positive Image'.format(i+1), 'Probe {:d} Negative Image'.format(i+1),
+                           'Intensity Difference',
+                           lognorm1=True, lognorm2=True, vmin1=Ip[i].max()/1e6, vmin2=In[i].max()/1e6,
+                          )
+            
+    E_probes = np.zeros((2*nmask*nwaves*nprobes,))
+    I_diff = np.zeros((nprobes*nmask,))
+    for i in range(nprobes):
+        I_diff[i*nmask:(i+1)*nmask] = (Ip[i] - In[i])[dark_mask]
+        
+#         E_probe_all_waves = jacobian.dot(np.array(probes[i].flatten()))
+#         E_probes[ i*2*nmask*nwaves : (i+1)*2*nmask*nwaves ] = E_probe_all_waves
+        
+        for j in range(nwaves):
+            jac_wave = jacobian[j*2*nmask:(j+1)*2*nmask]
+            E_probe = jac_wave.dot(np.array(probes[i].flatten()))
+            
+            E_probes[i*nwaves*2*nmask + j*2*nmask : i*nwaves*2*nmask + (j+1)*2*nmask] = E_probe
+#             E_probes = E_probe if i==0 and j==0 else np.concatenate((E_probes, E_probe))
+            
+#             E_probe_2d = np.zeros((sysi.npsf,sysi.npsf), dtype=np.complex128)
+# #             np.place(E_probe_2d, mask=dark_mask, vals=E_probe[:nmask]+1j*E_probe[nmask:])
+#             np.place(E_probe_2d, mask=dark_mask, 
+#                      vals=E_probes[(i+j)*2*nmask:(i+j+1)*2*nmask][:nmask] + 1j*E_probes[(i+j)*2*nmask:(i+j+1)*2*nmask][nmask:])
+#             misc.myimshow2(np.abs(E_probe_2d), np.angle(E_probe_2d))
+            
+    B = np.diag(np.ones((nmask,2*nmask))[0], k=0)[:nmask,:2*nmask] \
+        + np.diag(np.ones((nmask,2*nmask))[0], k=nmask)[:nmask,:2*nmask]
+    Bfull = np.tile(B, nwaves )
+    misc.myimshow(Bfull)
+    print('B.shape, Bfull.shape', B.shape, Bfull.shape)
+    
+    for i in range(nprobes):
+        h = 4 * Bfull @ np.diag( E_probes[ i*2*nmask*nwaves : (i+1)*2*nmask*nwaves ] )
+        Hinv = h1 if i==0 else np.vstack((Hinv,h1))
+        for j in range(nwaves):
+#             h = 4 * B @ np.diag( E_probes[(i+j)*2*nmask:(i+j+1)*2*nmask] )
+#             Hinv = h if i==0 and j==0 else np.hstack((Hinv,h))
+            
+            E_probe_2d = np.zeros((sysi.npsf,sysi.npsf), dtype=np.complex128)
+            np.place(E_probe_2d, mask=dark_mask, 
+                     vals=E_probes[i*nwaves*2*nmask + j*2*nmask : i*nwaves*2*nmask + (j+1)*2*nmask][:nmask] + \
+                          1j*E_probes[i*nwaves*2*nmask + j*2*nmask : i*nwaves*2*nmask + (j+1)*2*nmask][nmask:])
+            misc.myimshow2(np.abs(E_probe_2d), np.angle(E_probe_2d))
+#         Hinv = Hinv if i==0 else np.vstack((Hinv,Hinv))
+    print('Hinv.shape', Hinv.shape)
+    
+    H = np.linalg.inv(Hinv.T@Hinv)@Hinv.T
+    print('H.shape', H.shape)
+    
+    E_est = H.dot(I_diff)
+        
+    E_est_2d = np.zeros((sysi.npsf,sysi.npsf), dtype=np.complex128)
+    np.place(E_est_2d, mask=dark_mask, vals=E_est)
+    
+    return E_est_2d
 
 
 
