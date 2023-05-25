b7ed7409482a72a80300b2ba829446afe0264114
Kian Milani <kianmilani@arizona.edu>
2023-05-24 17:33:21 -0700

    testing more 2dm iefc and making SVC model

--- hcipy_models.py
+++ hcipy_models.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from IPython.display import clear_output
 from importlib import reload
 import time
+import copy
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -82,7 +83,7 @@ class PC(): # perfect coronagraph
         self.dm_mask[r>0.0105] = 0 # had to set the threshold to 10.5 instead of 10.2 to include edge actuators
         
 #         self.dm_zernikes = poppy.zernike.arbitrary_basis(self.dm_mask, nterms=15, outside=0)
-        self.dm_zernikes = poppy.zernike.arbitrary_basis(cp.array(self.dm_mask), nterms=15, outside=0).get()
+        self.dm_zernikes = poppy.zernike.arbitrary_basis(np.array(self.dm_mask), nterms=15, outside=0)
         
         
     def set_dm1(self, command):
@@ -135,7 +136,13 @@ class PC(): # perfect coronagraph
 class SVC(): # scalar vortex coronagraph
 
     def __init__(self, 
+                 npix = 256,
+                 oversample = 4,
+                 fgrid_sampling = 4,
+                 fgrid_extent = 16,
                  wavelength=None, 
+                 pupil_diam=2*u.m,
+                 fnum=20,
                  dm1_dm2=200e-3*u.m,
                  aberration_distance=100e-3*u.m,
                  influence_functions=None,
@@ -145,42 +152,57 @@ class SVC(): # scalar vortex coronagraph
         
         self.wavelength_c = 575e-9*u.m
         self.wavelength = self.wavelength_c if wavelength is None else wavelength
-        self.f = 500*u.mm
+        self.pupil_diam = pupil_diam
+        self.fnum = fnum
         
-        self.dm_diam = 10.2*u.mm
+        self.npix = npix
+        self.oversample = oversample
         self.dm1_dm2 = dm1_dm2
         
-        self.FN = (self.dm_diam**2/self.dm1_dm2/self.wavelength).decompose()
+        self.FN = self.pupil_diam.to_value(u.m)**2/self.dm1_dm2.to_value(u.m)/self.wavelength.to_value(u.m)
         
         # define the grids and the optics
-        self.npsf = 64
+        self.npix = npix
+        self.oversample = oversample
         
-        self.pupil_grid = hci.make_pupil_grid(256, 1.5)
-        self.focal_grid = hci.make_focal_grid(8, 12)
-        self.prop = hci.FraunhoferPropagator(self.pupil_grid, self.focal_grid)
+        self.fgrid_sampling = fgrid_sampling # number of pixels per resolution element (lambda*fnum)
+        self.fgrid_extent = fgrid_extent # number of resolution elements across grid (lambda*fnum)
+        self.npsf = 2*fgrid_sampling*fgrid_extent
         
-        self.aperture = hci.evaluate_supersampled(hci.circular_aperture(1), self.pupil_grid, 4)
-        self.lyot_mask = hci.evaluate_supersampled(hci.circular_aperture(0.95), self.pupil_grid, 4)
+        self.pupil_grid = hci.make_pupil_grid(int(npix*oversample), pupil_diam.to_value(u.m) * oversample)
+        self.focal_grid = hci.make_focal_grid(fgrid_sampling, fgrid_extent, 
+                                         pupil_diameter=self.pupil_diam.to_value(u.m),
+                                         focal_length=self.fnum * self.pupil_diam.to_value(u.m),
+                                         reference_wavelength=self.wavelength_c.to_value(u.m))
+        self.prop = hci.FraunhoferPropagator(self.pupil_grid, self.focal_grid,
+                                             focal_length=self.fnum*self.pupil_diam.to_value(u.m))
         
-        self.charge = charge
-        self.coro = hci.VortexCoronagraph(self.pupil_grid, self.charge)
-        self.lyot_stop = hci.Apodizer(self.lyot_mask)
-        
+        self.aperture = hci.evaluate_supersampled(hci.circular_aperture(self.pupil_diam.to_value(u.m)),
+                                                  self.pupil_grid, 
+                                                  self.oversample)
+        self.lyot_mask = hci.evaluate_supersampled(hci.circular_aperture(0.95*self.pupil_diam.to_value(u.m)), 
+                                                   self.pupil_grid, 
+                                                   self.oversample)
+
         # define the DMs
         self.Nact = 34
-        self.actuator_spacing = 1.00 / self.Nact * self.dm_diam.to_value(u.m)
+        self.actuator_spacing = 1.00 * self.pupil_diam.to_value(u.m)/self.Nact
         
         self.norm = norm
         
         # make the aberrations of the system (make sure to remove tip-tilt)
-        aberration_ptv = 0.02 * self.wavelength.to_value(u.m)
-        tip_tilt = hci.make_zernike_basis(3, self.dm_diam.to_value(u.m), self.pupil_grid, starting_mode=2)
-        wfe = hci.SurfaceAberration(self.pupil_grid, aberration_ptv, self.dm_diam.to_value(u.m), remove_modes=tip_tilt, exponent=-3)
-        self.aberration_distance_m = aberration_distance.to_value(u.m)
-        self.wfe_at_distance = hci.SurfaceAberrationAtDistance(wfe, self.aberration_distance_m)
+        aberration_ptv = 0.02 * self.wavelength_c.to_value(u.m)
+        tip_tilt = hci.make_zernike_basis(3, self.pupil_diam.to_value(u.m), self.pupil_grid, starting_mode=2)
+        wfe = hci.SurfaceAberration(self.pupil_grid, aberration_ptv, 
+                                    self.pupil_diam.to_value(u.m), remove_modes=tip_tilt, exponent=-3)
+
+        self.aberration_distance = aberration_distance
+        self.wfe_at_distance = hci.SurfaceAberrationAtDistance(wfe, self.aberration_distance.to_value(u.m))
         
         # make the coronagraph model
-        self.coronagraph = hci.PerfectCoronagraph(self.aperture, order=6)
+        self.charge = charge
+        self.coro = hci.VortexCoronagraph(self.pupil_grid, self.charge)
+        self.lyot_stop = hci.Apodizer(self.lyot_mask)
         
         if influence_functions is not None: 
             self.init_dms(influence_functions)
@@ -188,17 +210,17 @@ class SVC(): # scalar vortex coronagraph
     def init_dms(self, influence_functions):
         self.DM1 = hci.DeformableMirror(influence_functions)
         self.DM2 = hci.DeformableMirror(influence_functions)
-
+        
         self.prop_between_dms = hci.FresnelPropagator(self.pupil_grid, self.dm1_dm2.to_value(u.m))
         
-        self.dm_mask = np.ones((self.Nact,self.Nact), dtype=bool)
-        xx = (np.linspace(0, self.Nact-1, self.Nact) - self.Nact/2 + 1/2) * self.actuator_spacing*2
-        x,y = np.meshgrid(xx,xx)
-        r = np.sqrt(x**2 + y**2)
-        self.dm_mask[r>0.0105] = 0 # had to set the threshold to 10.5 instead of 10.2 to include edge actuators
+#         self.dm_mask = np.ones((self.Nact,self.Nact), dtype=bool)
+#         xx = (np.linspace(0, self.Nact-1, self.Nact) - self.Nact/2 + 1/2) * self.actuator_spacing*2
+#         x,y = np.meshgrid(xx,xx)
+#         r = np.sqrt(x**2 + y**2)
+#         self.dm_mask[r>0.0105] = 0 # had to set the threshold to 10.5 instead of 10.2 to include edge actuators
         
 #         self.dm_zernikes = poppy.zernike.arbitrary_basis(self.dm_mask, nterms=15, outside=0)
-        self.dm_zernikes = poppy.zernike.arbitrary_basis(cp.array(self.dm_mask), nterms=15, outside=0).get()
+#         self.dm_zernikes = poppy.zernike.arbitrary_basis(cp.array(self.dm_mask), nterms=15, outside=0).get()
         
         
     def set_dm1(self, command):
@@ -226,29 +248,55 @@ class SVC(): # scalar vortex coronagraph
     def show_dms(self):
         misc.imshow2(self.DM1.actuators.reshape(c.Nact, c.Nact),
                      self.DM2.actuators.reshape(c.Nact, c.Nact))
+    
+    def calc_wfs(self):
+        wfs = []
         
+        wf = hci.Wavefront(self.aperture, self.wavelength.to_value(u.m))
+        wfs.append(copy.copy(wf.real.shaped + 1j*wf.imag.shaped))
+        
+        wf = self.wfe_at_distance(wf)
+        wfs.append(copy.copy(wf.real.shaped + 1j*wf.imag.shaped))
+        
+        wf = self.DM1(wf)
+        wfs.append(copy.copy(wf.real.shaped + 1j*wf.imag.shaped))
+        
+        wf = self.DM2(self.prop_between_dms.forward(wf))
+        wfs.append(copy.copy(wf.real.shaped + 1j*wf.imag.shaped))
+        
+        wf = self.prop_between_dms.backward(wf)
+        wfs.append(copy.copy(wf.real.shaped + 1j*wf.imag.shaped))
+        
+        wf = self.coro(wf)
+        wfs.append(copy.copy(wf.real.shaped + 1j*wf.imag.shaped))
+        
+        wf = self.lyot_stop(wf)
+        wfs.append(copy.copy(wf.real.shaped + 1j*wf.imag.shaped))
+        
+        wf = self.prop(wf)
+        wfs.append(copy.copy(wf.real.shaped + 1j*wf.imag.shaped))
+        
+        return wfs
+    
     def calc_psf(self):
         wf = hci.Wavefront(self.aperture, self.wavelength.to_value(u.m))
-        wf = self.wfe_at_distance(wf) # apply aberrations to wavefront
-        wf = self.prop_between_dms.backward(self.DM2(self.prop_between_dms.forward(self.DM1(wf)))) # apply DMs
-        wf = self.prop(self.coronagraph(wf))
+        wf = self.wfe_at_distance(wf)
+        wf = self.prop_between_dms.backward(self.DM2(self.prop_between_dms.forward(self.DM1(wf))))
+        lyot_plane = self.coro(wf)
+        wf_post_lyot_mask = self.lyot_stop(lyot_plane)
+        wf_detector = self.prop(post_lyot_mask)
         psf_wf = wf.real.shaped + 1j*wf.imag.shaped
 
         return np.array(psf_wf)
 
     def snap(self):
-#         wf = hci.Wavefront(self.aperture, self.wavelength.to_value(u.m))
-#         wf = self.wfe_at_distance(wf) # apply aberrations to wavefront
-#         wf = self.prop_between_dms.backward(self.DM2(self.prop_between_dms.forward(self.DM1(wf)))) # apply DMs
-#         im = self.prop(self.coronagraph(wf))
-#         im = np.array(im.intensity.shaped)
-#         if self.norm is not None: 
-#             im /= self.norm
         
         wf = hci.Wavefront(self.aperture, self.wavelength.to_value(u.m))
-        lyot_plane = self.coro(wf)
-        post_lyot_mask = self.lyot_stop(lyot_plane)
-        im = self.prop(post_lyot_mask).intensity.shaped
+        wf = self.wfe_at_distance(wf)
+        wf = self.prop_between_dms.backward(self.DM2(self.prop_between_dms.forward(self.DM1(wf))))
+        wf = self.coro(wf)
+        wf = self.lyot_stop(wf)
+        im = self.prop(wf).intensity.shaped
         
         return im
     
--- spc_wide_iefc_calibration.py
+++ spc_wide_iefc_calibration.py
@@ -1,10 +1,14 @@
 import numpy as np
+import scipy
+
 import cupy as cp
+import cupyx.scipy
+
 import astropy.units as u
 from astropy.io import fits
 from matplotlib.patches import Rectangle, Circle
 from pathlib import Path
-from IPython.display import clear_output
+from IPython.display import display, clear_output
 from importlib import reload
 import time
 
@@ -18,76 +22,109 @@ poppy_log.disabled = True
 import warnings
 warnings.filterwarnings("ignore")
 
+import wfsc_tests as wfsc
+from wfsc_tests.math_module import xp, _scipy
+wfsc.math_module.update_np(cp)
+wfsc.math_module.update_scipy(cupyx.scipy)
+
 import cgi_phasec_poppy as cgi
-from cgi_phasec_poppy import misc
 
-from wfsc import iefc_2dm as iefc
-from wfsc import utils
+import misc_funs as misc
 
-dm_dir = cgi.data_dir/'dm-acts'
+iefc_dir = Path('/groups/douglase/kians-data-files/roman-cgi-iefc-data')
 
-sysi = cgi.CGI(cgi_mode='spc-wide', npsf=150,
-              use_fpm=True,
-              use_pupil_defocus=False, 
-              polaxis=0,
+source_flux = 2.0208517e8 * u.ph/u.s/u.m**2 # flux of 47 UMa at 575nm with 10% bandpass
+
+c = cgi.CGI(cgi_mode='spc-wide', npsf=150, 
+              use_pupil_defocus=True, 
               use_opds=True,
-             )
-sysi.show_dms()
+              source_flux=source_flux,
+            exp_time=5)
 
-npsf = sysi.npsf
-Nact = sysi.Nact
+npsf = c.npsf
+Nact = c.Nact
 
-xfp = (np.linspace(-npsf/2, npsf/2-1, npsf) + 1/2)*sysi.psf_pixelscale_lamD
-fpx,fpy = np.meshgrid(xfp,xfp)
-fpr = np.sqrt(fpx**2 + fpy**2)
-    
-iwa = 5.5
-owa = 20.5
-regions = [iwa, 6, 10, 20, owa]
-weights = [0.1, 0.8, 1, 0.1]
-weight_map = np.zeros((sysi.npsf,sysi.npsf), dtype=np.float64)
-for i in range(len(weights)):
-    roi_params = {
-        'inner_radius' : regions[i],
-        'outer_radius' : regions[i+1],
-        'edge_position' : 0,
+c.use_fpm = False
+ref_unocc_im = c.snap()
+
+max_ref = ref_unocc_im.get().max()
+display(max_ref)
+
+c.use_fpm = True
+c.source_flux = source_flux/max_ref # divide the source flux to get nominal contrast images
+ref_im = c.snap()
+
+reload(wfsc)
+reload(wfsc.utils)
+reload(wfsc.imshows)
+xfp = (xp.linspace(-npsf/2, npsf/2-1, npsf) + 1/2)*c.psf_pixelscale_lamD
+fpx,fpy = xp.meshgrid(xfp,xfp)
+  
+iwa = 6
+owa = 20
+roi_params = {
+        'inner_radius' : iwa,
+        'outer_radius' : owa,
+#         'edge' : 2,
         'rotation':0,
         'full':True,
     }
-    roi = utils.create_annular_focal_plane_mask(fpx, fpy, roi_params)
-    weight_map += roi*weights[i]
+roi1 = wfsc.utils.create_annular_focal_plane_mask(fpx, fpy, roi_params, plot=False)
 
+iwa = 6
+owa = 20
+roi_params = {
+        'inner_radius' : iwa,
+        'outer_radius' : owa,
+#         'edge' : 0,
+        'rotation':0,
+        'full':True,
+    }
+roi2 = wfsc.utils.create_annular_focal_plane_mask(fpx, fpy, roi_params, plot=False)
+
+# wfsc.imshow2(roi1, roi2)
+relative_weight = 0.2
+weight_map = roi1 + relative_weight*(roi2 * ~roi1)
 control_mask = weight_map>0
-misc.myimshow2(weight_map, control_mask)
 
-probe_amp = 3e-8
+reload(wfsc.utils)
+calib_amp = 5e-9
+fourier_modes, fs = wfsc.utils.select_fourier_modes(c, control_mask*(fpx>0), fourier_sampling=0.8) 
+nmodes = fourier_modes.shape[0]
+print(fourier_modes.shape, fs.shape)
+
+probe_modes = wfsc.utils.create_probe_poke_modes(Nact, 
+                                                 poke_indices=[(Nact//3, Nact//3), 
+                                                               (2*Nact//3, Nact//3), 
+                                                               (Nact//2, 2*Nact//3)], 
+                                                 plot=False)
+
+probe_amp = 5e-8
 calib_amp = 5e-9
 
-fourier_modes, fs = utils.select_fourier_modes(sysi, control_mask*(fpx>0), fourier_sampling=0.85) 
-nf = fourier_modes.shape[0]
-print(fourier_modes.shape)
+differential_images, single_images = wfsc.iefc_2dm.take_measurement(c, 
+                                                                   probe_modes, probe_amp, 
+                                                                   return_all=True)
+ims = differential_images.reshape(3, c.npsf, c.npsf)
 
-calib_modes = np.concatenate((fourier_modes, fourier_modes), axis=1)
+response_matrix, response_cube = wfsc.iefc_2dm.calibrate(c, 
+                                                         control_mask.ravel(),
+                                                         probe_amp, probe_modes, 
+                                                         calib_amp, fourier_modes, 
+                                                         return_all=True)
 
-# had_modes = utils.get_hadamard_modes(sysi.dm_mask)[:1024]
-# nh = had_modes.shape[0]
-# print(had_modes.shape)
-    
-probe_modes = utils.create_probe_poke_modes(Nact, xinds=[Nact//4, Nact//4+1], yinds=[Nact//4, Nact//4], display=True)
-    
-sysi.reset_dms()
-response_cube, calibration_cube = iefc.calibrate(sysi, 
-                                                 probe_amp, probe_modes, 
-                                                 calib_amp, 
-                                                 calib_modes,
-#                                                  fourier_modes, had_modes,
-#                                                  cos_modes, sin_modes,
-                                                )
+misc.save_fits(iefc_dir/'response-data'/'spcwide_iefc_2dm_response_matrix_20230524.fits', 
+               wfsc.utils.ensure_np_array(response_matrix))
+misc.save_fits(iefc_dir/'response-data'/'spcwide_iefc_2dm_response_cube_20230524.fits', 
+               wfsc.utils.ensure_np_array(response_cube))
 
-fname = 'spc-wide_2dm_annular_2poke_fourier.pkl'
-iefc_dir = Path('/groups/douglase/kians-data-files/roman-cgi-iefc-data')
+response_sum = xp.sum(abs(response_cube), axis=(0,1))
+wfsc.imshow1(response_sum.reshape(npsf, npsf), lognorm=True)
 
-misc.save_pickle(iefc_dir/'response-data'/fname, response_cube)
-misc.save_pickle(iefc_dir/'calibration-data'/fname, calibration_cube)
+
+
+
+
+
 
 
