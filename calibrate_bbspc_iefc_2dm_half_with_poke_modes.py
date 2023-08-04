54cad299099c127e751f7143edf3150bfff1f5a9
Kian Milani <kianmilani@arizona.edu>
2023-08-04 13:16:51 -0700

    trying braodband spc sim with noise

---
+++ calibrate_bbspc_iefc_2dm_half_with_poke_modes.py
@@ -0,0 +1,157 @@
+import numpy as np
+import scipy
+
+import cupy as cp
+import cupyx.scipy
+
+import astropy.units as u
+from astropy.io import fits
+from matplotlib.patches import Rectangle, Circle
+from pathlib import Path
+from IPython.display import clear_output, display, HTML
+display(HTML("<style>.container { width:90% !important; }</style>")) # just making the notebook cells wider
+from datetime import datetime
+today = int(datetime.today().strftime('%Y%m%d'))
+
+from importlib import reload
+import time
+
+import logging, sys
+poppy_log = logging.getLogger('poppy')
+poppy_log.setLevel('DEBUG')
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+poppy_log.disabled = True
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import cgi_phasec_poppy
+
+import ray
+if not ray.is_initialized():
+    ray.init(log_to_driver=False)
+    
+from math_module import xp, ensure_np_array
+import iefc_2dm 
+import utils
+from imshows import *
+
+data_dir = iefc_2dm.iefc_data_dir
+response_dir = data_dir/'response-data'
+
+# Define the wavelengths and flux values
+wavelength_c = 825e-9*u.m
+
+nlam = 3
+bandwidth = 2.9/100
+minlam = wavelength_c * (1 - bandwidth/2)
+maxlam = wavelength_c * (1 + bandwidth/2)
+wavelengths = np.linspace( minlam, maxlam, nlam )
+
+from astropy.constants import h, c, k_B, R_sun
+
+zpup = cgi_phasec_poppy.source_flux.SOURCE(wavelengths=wavelengths,
+                                            temp=40000*u.K,
+                                            distance=300*u.parsec,
+                                            diameter=2*14*R_sun,
+                                            name='$\zeta$ Pup', 
+                                            lambdas=np.linspace(10, 1000, 19801)*u.nm,
+                                           )
+
+zpup.plot_spectrum()
+# zpup.plot_spectrum_ph()
+
+source_fluxes = zpup.calc_fluxes()
+print(source_fluxes)
+total_flux = np.sum(source_fluxes)
+print(total_flux)
+
+# define the ray actors
+rayCGI = ray.remote(cgi_phasec_poppy.cgi.CGI) # make a ray actor class from the original CGI class  
+
+kwargs = {
+    'cgi_mode':'spc-wide',
+    'npsf':148,
+    'use_pupil_defocus':True,
+    'use_opds':True,
+    'polaxis':10,
+}
+
+actors = []
+for i in range(nlam):
+    actors.append(rayCGI.options(num_cpus=2, num_gpus=1/8).remote(**kwargs))
+    actors[i].setattr.remote('wavelength', wavelengths[i])
+    actors[i].setattr.remote('source_flux', source_fluxes[i])
+
+reload(cgi_phasec_poppy.parallelized_cgi)
+mode = cgi_phasec_poppy.parallelized_cgi.ParallelizedCGI(actors=actors)
+
+mode.use_noise = True
+mode.exp_time = 10*u.s
+mode.gain = 10
+mode.dark_current_rate = 0.05*u.electron/u.pix/u.hour
+# mode.dark_current_rate = 0.0*u.electron/u.pix/u.hour
+mode.read_noise = 120*u.electron/u.pix
+# mode.read_noise = 0*u.electron/u.pix
+
+mode.set_actor_attr('use_fpm',False)
+ref_unocc_im = mode.snap()
+imshow1(ref_unocc_im, pxscl=mode.psf_pixelscale_lamD, xlabel='$\lambda/D$', lognorm=True)
+
+max_ref = xp.max(ref_unocc_im)
+print(max_ref)
+
+mode.set_actor_attr('use_fpm',True)
+mode.Imax_ref = max_ref
+mode.exp_time_ref = mode.exp_time
+mode.gain_ref = mode.gain
+
+mode.exp_time = 10*u.s
+mode.gain = 10
+
+ref_im = mode.snap()
+imshow1(ref_im, pxscl=mode.psf_pixelscale_lamD, xlabel='$\lambda/D$', lognorm=True)
+
+
+# Make the ROI
+reload(utils)
+roi1 = utils.create_annular_focal_plane_mask(mode, inner_radius=6, outer_radius=20, edge=None, plot=True)
+roi2 = utils.create_annular_focal_plane_mask(mode, inner_radius=5.4, outer_radius=20.6, edge=None, plot=True)
+roi3 = utils.create_annular_focal_plane_mask(mode, inner_radius=6, outer_radius=11, edge=None, plot=True)
+
+relative_weight_1 = 0.9
+relative_weight_2 = 0.4
+weight_map = roi3 + relative_weight_1*(roi1*~roi3) + relative_weight_2*(roi2*~roi1*~roi3)
+control_mask = weight_map>0
+imshow2(weight_map, control_mask*ref_im, lognorm2=True)
+mean_ni = xp.mean(ref_im[control_mask])
+print(mean_ni)
+
+# Create probe modes
+probe_amp = 2.5e-8
+probe_modes = utils.create_fourier_probes(mode, control_mask, fourier_sampling=0.25, shift=(-10,8), nprobes=3, plot=True)
+
+# Create calibration modes
+calib_amp = 5e-9
+calib_modes = utils.create_all_poke_modes(mode.dm_mask)
+
+response_matrix, response_cube = iefc_2dm.calibrate(mode, 
+                                                    control_mask,
+                                                    probe_amp, probe_modes, 
+                                                    calib_amp, calib_modes, 
+                                                    return_all=True)
+
+utils.save_fits(response_dir/f'bbspc_iefc_2dm_poke_response_matrix_{today}.fits', ensure_np_array(response_matrix))
+utils.save_fits(response_dir/f'bbspc_iefc_2dm_poke_response_cube_{today}.fits', ensure_np_array(response_cube))
+
+
+
+
+
+
+
+
+
+
+
