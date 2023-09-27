fc72fdb5f9a4f74965b3aebedf2f9a2067128526
Kian Milani <kianmilani@email.arizona.edu>
2023-09-26 21:15:38 -0700

    fourier modes do not work as well

---
+++ iefc-scripts/iefc_2dm_spc_wide_825_fourier_modes.py
@@ -0,0 +1,115 @@
+import sys
+sys.path.append("..")
+
+import numpy as np
+import scipy
+
+import cupy as cp
+import cupyx.scipy
+
+import astropy.units as u
+from astropy.io import fits
+from matplotlib.patches import Rectangle, Circle
+from pathlib import Path
+from IPython.display import clear_output, display, HTML
+display(HTML("<style>.container { width:90% !important; }</style>")) # just making the notebook cells wider
+from datetime import datetime
+today = int(datetime.today().strftime('%Y%m%d'))
+
+from importlib import reload
+import time
+
+import logging, sys
+poppy_log = logging.getLogger('poppy')
+poppy_log.setLevel('DEBUG')
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+poppy_log.disabled = True
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import cgi_phasec_poppy
+
+import ray
+if not ray.is_initialized():
+    ray.init(log_to_driver=False)
+    
+from math_module import xp, ensure_np_array
+import iefc_2dm 
+import utils
+from imshows import *
+
+data_dir = iefc_2dm.iefc_data_dir
+response_dir = data_dir/'response-data'
+
+
+mode = cgi_phasec_poppy.cgi.CGI(cgi_mode='spc-wide', npsf=150,
+                                  use_pupil_defocus=True, 
+                                  use_opds=True)
+mode.use_fpm = False
+ref_unocc_im = mode.snap()
+imshow1(ref_unocc_im, pxscl=mode.psf_pixelscale_lamD, xlabel='$\lambda/D$', lognorm=True)
+
+mode.Imax_ref = ref_unocc_im.get().max()
+mode.use_fpm = True
+
+ref_im = mode.snap()
+imshow1(ref_im, 'SPC-WFOV Initial Coronagraphic Image\nfor iEFC',
+        pxscl=mode.psf_pixelscale_lamD, xlabel='$\lambda/D$', lognorm=True, vmin=1e-11)
+
+reload(utils)
+roi1 = utils.create_annular_focal_plane_mask(mode, inner_radius=6, outer_radius=20, edge=None, plot=True)
+roi2 = utils.create_annular_focal_plane_mask(mode, inner_radius=5.4, outer_radius=20.6, edge=None, plot=True)
+roi3 = utils.create_annular_focal_plane_mask(mode, inner_radius=6, outer_radius=11, edge=None, plot=True)
+
+relative_weight_1 = 0.9
+relative_weight_2 = 0.4
+weight_map = roi3 + relative_weight_1*(roi1*~roi3) + relative_weight_2*(roi2*~roi1*~roi3)
+control_mask = weight_map>0
+imshow2(weight_map, control_mask*ref_im, lognorm2=True, save_fig='test_fig.png')
+mean_ni = xp.mean(ref_im[control_mask])
+print(mean_ni)
+
+reload(utils)
+probe_amp = 2.5e-8
+# probe_modes = utils.create_fourier_probes(mode, control_mask, fourier_sampling=0.2, shift=[(-12,6), (12,6), (0,-12)], nprobes=3, plot=True)
+probe_modes = utils.create_poke_probes([(10,34), (38,34), (24,10)], plot=True)
+# probe_modes = utils.create_poke_probes([(23,9), (25,9), (24,10)], plot=True)
+imshow3(probe_modes[0], probe_modes[1], probe_modes[2], save_fig='probes.png')
+utils.save_fits(response_dir/f'spc_wide_825_poke_probes_{today}.fits', probe_modes)
+
+fourier_mask = utils.create_annular_focal_plane_mask(mode, inner_radius=5.4, outer_radius=20.6, edge=0, plot=True)
+calib_modes = utils.create_fourier_modes(mode, fourier_mask, fourier_sampling=0.85, ndms=2)
+Nfourier = calib_modes.shape[0]//2
+print(calib_modes.shape)
+i = 100
+imshow2(calib_modes[i,:mode.Nact**2].reshape(mode.Nact,mode.Nact),
+        calib_modes[i+Nfourier,mode.Nact**2:].reshape(mode.Nact,mode.Nact),
+        save_fig='test_fourier_modes.png')
+
+
+calib_amp = 2.5e-9
+
+response_matrix, response_cube = iefc_2dm.calibrate(mode, 
+                                                    control_mask,
+                                                    probe_amp, probe_modes, 
+                                                     calib_amp, calib_modes, 
+                                                     return_all=True, 
+#                                                     plot_responses=False,
+                                                   )
+
+
+utils.save_fits(response_dir/f'spc_wide_825_fourier_modes_response_matrix_2.5_{today}.fits', response_matrix)
+utils.save_fits(response_dir/f'spc_wide_825_fourier_modes_response_cube_2.5_{today}.fits', response_cube)
+
+
+
+
+
+
+
+
+
+
+
